77dba5c03a67e18d94978dce5d7161add7ea0732
MajorPlayz <alistairsmith01@gmail.com>
2016-10-28 10:30:25 +0100

    Starting

---
+++ src/majorplayz/main.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace majorplayz;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\Player;
+use pocketmine\event\Listener;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat;
+use pocketmine\level\Level;
+use pocketmine\level\Position;
+use pocketmine\math\Vector3;
+
+class Main extends PluginBase implements Listener {
+	
+	// Constnants:
+	const AUTHOR = "MajorPlayz";
+	const PREFIX = "Core:";
+	
+	public function onEnable() {
+		$this->getLogger ()->info ( TextFormat::GREEN . "Majorcraft Core Started" );
+		$this->saveDefaultConfig ();
+		$this->reloadConfig ();
+	}
+	
+	public function onDisable() {
+		$this->getLogger ()->info ( TextFormat::GREEN . "SuperSpawners Disabled... Did the server stop?" );
+	}
+	
+	public function onLoad() {
+		$this->getLogger ()->info ( TextFormat::GREEN . "SuperSpawners Loaded" );
+	}
+	
+}
+?>
